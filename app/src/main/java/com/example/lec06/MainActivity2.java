15f07b4e48296e4c188e51a982fc27be163da6df
Dani <mcsf19a046@pucit.edu.pk>
2021-06-06 04:10:17 -0700

    we add some new feature.

--- app/src/main/java/com/example/lec06/MainActivity2.java
+++ app/src/main/java/com/example/lec06/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.lec06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void GotoDialer(View view) {
+        Uri uri= Uri.parse("tel:+92 3078636043");
+        Intent intent=new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+
+    }
+
+    public void Gotowebside(View view) {
+        Uri uri= Uri.parse("https://github.com/adnan657/Lec_06-new-activity-insert-");
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        startActivity(intent);
     }
 }
